b84399ea6ccc5fd8f432e7412926f4ecd5246fea
rubenssilva <rubsandsu@gmail.com>
2019-08-19 17:42:31 -0300

    adicionando sons de aplauso ao vencer

--- components/DialogWinner/index.js
+++ components/DialogWinner/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
 
 import Dialog, {
   SlideAnimation,
@@ -10,9 +10,53 @@ import Dialog, {
   DialogButton
 } from "react-native-popup-dialog";
 
-import PreImage from "../PreImage";
+import Anime from "react-native-anime";
 
 export default class DialogWinner extends Component {
+  componentDidMount() {
+    this.makeAlarm();
+  }
+
+  componentDidUpdate() {
+    if (this.props.visible) {
+      this.playAlarm();
+    }
+  }
+
+  componentWillUnmount() {
+    this.pauseAlarm();
+  }
+
+  // === alarm stuf ===
+
+  pauseAlarm = async () => {
+    const alarme = this.state.alarm;
+    try {
+      await alarme.pauseAsync();
+    } catch (error) {
+      console.log("(pause alarm) ERRO! Nao parou de tocar");
+    }
+  };
+
+  playAlarm = async () => {
+    const alarme = this.state.alarm;
+    try {
+      await alarme.playAsync();
+    } catch (error) {
+      console.log("(play alarm) Erro ao tocar o alarme");
+    }
+  };
+
+  makeAlarm = async () => {
+    const soundObject = new Expo.Audio.Sound();
+    try {
+      await soundObject.loadAsync(require("../../assets/sounds/claps.mp3"));
+      this.setState({ alarm: soundObject });
+    } catch (error) {
+      console.log("make alarm) Erro ao criar o alarme");
+    }
+  };
+
   render() {
     const {
       styles,
@@ -36,8 +80,20 @@ export default class DialogWinner extends Component {
           dialogTitle={<DialogTitle title={title} />}
           footer={
             <DialogFooter>
-              <DialogButton text="Cancelar" onPress={onCancelAction} />
-              <DialogButton text="OK" onPress={onOkAction} />
+              <DialogButton
+                text="Cancelar"
+                onPress={() => {
+                  this.pauseAlarm();
+                  onCancelAction();
+                }}
+              />
+              <DialogButton
+                text="OK"
+                onPress={() => {
+                  this.pauseAlarm();
+                  onOkAction();
+                }}
+              />
             </DialogFooter>
           }
         >
@@ -51,25 +107,17 @@ export default class DialogWinner extends Component {
                 }
               ]}
             >
-              <View>
-                <PreImage
-                  thumbnailSource={require("../../assets/images/threedots.png")}
-                  source={require("../../assets/images/trofeu.png")}
+              <Anime.View ref={ref => (this.img = ref)} onClick={this.onClick}>
+                <Image
                   style={{
-                    width: 100,
-                    height: 100,
-                    resizeMode: "contain"
+                    width: styles.heightScreen * 0.18,
+                    height: styles.heightScreen * 0.18,
+                    resizeMode: "contain",
+                    marginBottom: 15
                   }}
+                  source={require("../../assets/images/trofeu.png")}
                 />
-                {/* <Image
-                style={{
-                  width: 100,
-                  height: 100,
-                  resizeMode: "contain"
-                }}
-                source={require("../assets/images/trofeu.png")}
-              /> */}
-              </View>
+              </Anime.View>
 
               <ScrollView style={{ minHeight: 80 }}>
                 <Text
